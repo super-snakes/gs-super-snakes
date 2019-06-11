89c0234616a62b8e5b028bb70ac11e3efde0fa37
Alyona <alyona.rodin@gmail.com>
2019-06-11 19:53:50 -0400

    created api user routes

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,13 +1,17 @@
 const User = require('./user')
 const Product = require('./product')
-
+const db = require('../db')
+const Order = require('./order')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
  *
  *    BlogPost.belongsTo(User)
  */
-
+Order.belongsToMany(Product, {through: 'OrderProduct'})
+Product.belongsToMany(Order, {through: 'OrderProduct'})
+User.hasMany(Order)
+Order.belongsTo(User)
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -16,6 +20,8 @@ const Product = require('./product')
  */
 
 module.exports = {
+  db,
+  Order,
   User,
   Product
 }
