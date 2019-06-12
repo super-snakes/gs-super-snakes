f5284e097d195f8b470737a6a700ddb8376d167c
Alex Mann <Alexander_Mann@alumni.brown.edu>
2019-06-12 15:04:10 -0400

    fixed Login page routing, changed validations for name on User

--- client/routes.js
+++ client/routes.js
@@ -20,7 +20,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/" component={Products} />
+        <Route exact path="/" component={Products} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route exact path="/cart" component={CartView} />
