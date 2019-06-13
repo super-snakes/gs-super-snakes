c795ede6291b45d503855c7e169be3b279dea9aa
Alex Mann <Alexander_Mann@alumni.brown.edu>
2019-06-13 12:13:40 -0400

    feat after login, user redirected to home page, which has user user info, and working logout button

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,34 +1,63 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import {logout} from '../store'
+import {Redirect} from 'react-router-dom'
 
 /**
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  const {user, handleClick} = props
+
+  if (!user.id) {
+    return <Redirect to="/" />
+  }
 
   return (
-    <div>
-      <h3>Welcome, {email}</h3>
+    <div className="h100 w100 flex column align-items-center justify-center">
+      <div className="flex">
+        <img src={user.imageUrl} className="rounded mr1" />
+        <h1>Welcome back {user.email}!</h1>
+      </div>
+      <div>
+        <button className="btn bg-red white p1 rounded" onClick={handleClick}>
+          Logout
+        </button>
+      </div>
     </div>
   )
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    user: state.user
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const history = ownProps.history
+  return {
+    handleClick() {
+      dispatch(logout()).then(() => history.push('/'))
+    }
+  }
 }
 
 /**
  * CONTAINER
  */
-const mapState = state => {
-  return {
-    email: state.user.email
-  }
-}
+// const mapState = state => {
+//   return {
+//     email: state.user.email
+//   }
+// }
 
-export default connect(mapState)(UserHome)
+export default connect(mapStateToProps, mapDispatchToProps)(UserHome)
 
 /**
  * PROP TYPES
  */
-UserHome.propTypes = {
-  email: PropTypes.string
-}
+// UserHome.propTypes = {
+//   email: PropTypes.string
+// }
