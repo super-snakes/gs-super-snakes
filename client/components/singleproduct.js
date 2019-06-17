f6d22d54df316183b7475ea8685e6bf0a195f17e
Alyona <alyona.rodin@gmail.com>
2019-06-17 16:10:43 -0400

    bug: uncommented the isAdmin update

--- client/components/singleproduct.js
+++ client/components/singleproduct.js
@@ -96,11 +96,13 @@ class SingleProduct extends React.Component {
             ) : (
               <h3>Price: ${price}</h3>
             )}
-            {/* {this.props.user.isAdmin ? ( */}
-            <button onClick={this.toggle_update_product}>Update Product</button>
-            {/* ) : (
+            {this.props.user.isAdmin ? (
+              <button onClick={this.toggle_update_product}>
+                Update Product
+              </button>
+            ) : (
               false
-            )} */}
+            )}
 
             {this.state.showUpdateProduct ? (
               <UpdateBookForm updateProductAction={this.updateProductAction} />
