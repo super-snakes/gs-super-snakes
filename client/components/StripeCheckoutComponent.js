dd94f06caaf6fe6d23cf66b9950bcaeb31d1b71a
sam-peach <sampeachart@gmail.com>
2019-06-17 17:51:06 -0400

    feat: working on stripe

--- client/components/StripeCheckoutComponent.js
+++ client/components/StripeCheckoutComponent.js
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom'
 import StripeCheckout from 'react-stripe-checkout'
 import axios from 'axios'
 import {toast} from 'react-toastify'
+
 // import 'react-toastify/dist/ReactToastify.css'
 
 toast.configure()
 
 const StripeCheckoutComponent = props => {
   async function handleToken(token, addresses) {
-    const response = await axios.post(
-      'https://ry7v05l6on.sse.codesandbox.io/checkout',
-      {token, product}
-    )
+    const response = await axios.post('/api/stripe', {token, product})
     const {status} = response.data
     console.log('Response:', response.data)
     if (status === 'success') {
