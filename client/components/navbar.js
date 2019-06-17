693b99293d856f5fb0da9fa6579fef0f02dadfe6
Mercedes <mercedesgmiller92@gmail.com>
2019-06-17 17:45:55 -0400

    feat: navbar is sticky

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -10,12 +10,8 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import OauthLoginForm from './oauth-login-form'
 import Button from '@material-ui/core/Button'
 
-// import {stat} from 'fs'
-
 const StyledBadge = withStyles(theme => ({
   badge: {
-    top: '50%',
-    right: -3,
     // The border color match the background color.
     border: `2px solid ${
       theme.palette.type === 'light'
@@ -33,89 +29,50 @@ const Navbar = (props, {handleClick, isLoggedIn}) => {
     }
   }
   return (
-    <div style={{backgroundColor: '#2F4F4F'}}>
+    <div id="nav-bar-wrapper">
       <Link to="/">
-        <span>
-          <h1
-            style={{
-              fontFamily: 'Comic Sans MS',
-              fontSize: '36px',
-              margin: '20px',
-              marginTop: '10px',
-              color: 'white'
-            }}
-          >
-            Super Snakes Bookstore
-          </h1>
+        <span id="home-link-wrapper">
+          <h1 id="super-snake">Super Snakes Bookstore</h1>
           <img
+            id="super-snake-logo"
             src="https://png.pngtree.com/element_origin_min_pic/17/08/15/3cfdfa77493149f83c91de3e69e1dcaf.jpg"
-            style={{
-              width: '100px',
-              height: '125px',
-              position: 'absolute',
-              right: '25px',
-              border: '3px solid #73AD21',
-              padding: '10px'
-            }}
           />
         </span>
       </Link>
       <nav>
         {props.user.id ? (
-          <div>
+          <div className="navbar-link-wrapper">
             {/* The navbar will show these links after you log in */}
-            <Link
-              to="/home"
-              style={{
-                color: 'white'
-              }}
-            >
-              Home
+            <Link className="nav-link" to="/home">
+              Profile
+            </Link>
+            <Link to="/cart" id="cart-wrapper">
+              <IconButton id="cartIcon" aria-label="Cart">
+                <StyledBadge badgeContent={cartSize} color="primary">
+                  <ShoppingCartIcon />
+                </StyledBadge>
+              </IconButton>
             </Link>
           </div>
         ) : (
-          <div>
-            <Link
-              to="/login"
-              style={{
-                color: 'white'
-              }}
-            >
+          <div className="navbar-link-wrapper">
+            <Link className="nav-link" to="/login">
               Login
             </Link>
 
-            <Link
-              to="/signup"
-              style={{
-                color: 'white'
-              }}
-            >
+            <Link className="nav-link" to="/signup">
               Sign Up
+            </Link>
+            <Link to="/cart" id="cart-wrapper">
+              <IconButton id="cartIcon">
+                <StyledBadge badgeContent={cartSize} color="primary">
+                  <ShoppingCartIcon />
+                </StyledBadge>
+              </IconButton>
             </Link>
           </div>
         )}
-        <span>
-          <Link to="/cart">
-            <IconButton
-              aria-label="Cart"
-              style={{
-                display: 'inline'
-              }}
-            >
-              <StyledBadge badgeContent={cartSize} color="primary">
-                <ShoppingCartIcon />
-              </StyledBadge>
-            </IconButton>
-          </Link>
-          <OauthLoginForm
-            style={{
-              display: 'inline',
-              padding: '10px'
-            }}
-          />
-        </span>
       </nav>
-      <hr />
     </div>
   )
 }
@@ -147,4 +104,21 @@ export default connect(mapState, mapDispatch)(Navbar)
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
+}
+
+{
+  /* <span>
+<Link to="/cart">
+  <IconButton
+    aria-label="Cart"
+    style={{
+      display: 'inline'
+    }}
+  >
+    <StyledBadge badgeContent={cartSize} color="primary">
+      <ShoppingCartIcon />
+    </StyledBadge>
+  </IconButton>
+</Link>
+</span> */
 }
